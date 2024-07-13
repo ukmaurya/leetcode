40d85846ddc787129939bb06e64fb6377dc8dec4
ukmaurya <ukmaurya30@gmail.com>
2024-07-13 17:54:40 +0530

    Time: 10 ms (35.30%) | Memory: 12.1 MB (19.09%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -22,11 +22,13 @@ class Solution {
     }
 public:
     string shortestPalindrome(string str) {
-        
+        // we have to find longest prefix plaindrome 
          int n = str.size();
         string temp = str;
         reverse(temp.begin(), temp.end());
-        string s = str + '$' + temp; // $ is added to beak continuity , ex aaaa 
+        string s = str + '$' + temp; // $ is added to break continuity , ex aaaa 
+        
+        // after adding the string in reverse , let lps=x will tell that x prefix part of string is equal to its reverse x part => x prefix part is palindrome
         vector<int> lps(s.size(), 0);
         calLPS(s, lps);
         int addLength = n - lps[s.size() - 1];
