96c3947f206261b5e29afceb15d5af21c35ea35e
ukmaurya <ukmaurya30@gmail.com>
2024-07-14 11:40:03 +0530

    Time: 4 ms (75.00%) | Memory: 29.4 MB (100.00%) - LeetSync

---
+++ 3494-minimum-cost-for-cutting-cake-i/minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,37 @@
+#define ll long long 
+class Solution {
+public:
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+          
+          sort(horizontalCut.begin() , horizontalCut.end() , greater<int>());
+          sort(verticalCut.begin() , verticalCut.end() , greater<int>());
+          int horPiece = 1;
+          int verPiece = 1;
+          int hptr = 0;
+          int vptr = 0;
+          ll total = 0;
+          while(hptr<m-1 && vptr<n-1){
+              if(horizontalCut[hptr]>verticalCut[vptr]){
+                   total += verPiece*horizontalCut[hptr];
+                   horPiece++;
+                   hptr++;
+              }
+              else{
+                     total += horPiece*verticalCut[vptr];
+                     verPiece++;
+                     vptr++;
+              }
+          }
+         while(hptr<m-1){
+             total += verPiece*horizontalCut[hptr];
+             horPiece++;
+             hptr++;
+         }
+         while( vptr<n-1){
+            total += horPiece*verticalCut[vptr];
+            verPiece++;
+            vptr++;
+         }
+        return total;
+    }
+};
