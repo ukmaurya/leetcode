170ea71e0e6f3ed36ae73301d2efae6dc14e8bbf
ukmaurya <ukmaurya30@gmail.com>
2024-07-26 17:26:34 +0530

    Time: 2 ms (62.16%) | Memory: 7.4 MB (13.63%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -14,13 +14,27 @@ class Solution {
         }
         return catNum[n] = ans;
     }
+ int catalan(int n)
+{
+    int res = 1;
+     for (int i = 1; i <= n; i++) { // using reduction of formula 
+        res = (res *1LL*(4 * i - 2)) / (i + 1);
+       
+       }
+    return res;
+}
 public:
     int numTrees(int n) {
         // it can be solved using 1)  catalan number in o(n) time 
         // 2) dp o(n^2) time 
+       /*. 
        vector<int> catNum(n+1,-1);
        int res = catalanNumber(n,catNum);
        return res;
+       */
+
+       return catalan(n);
+
 
 
     }
