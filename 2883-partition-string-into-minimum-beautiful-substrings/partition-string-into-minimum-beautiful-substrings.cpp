62a68558ff19c15aa0a050be732e07038fd3e261
ukmaurya <ukmaurya30@gmail.com>
2024-08-27 15:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (43.09%) - LeetSync

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool notleadZero(string &s , int start , int cur){
+        if(s[start]=='0')
+          return false;
+        return true;
+    }
+    bool power5(string &s , int start , int cur){
+        string temp = s.substr(start , cur-start+1);
+        if(temp=="")
+            return false;
+        long long  num = stoll(temp, nullptr, 2);
+        while(num>1){
+             if(num % 5 != 0) return false;
+              num = num/5;
+        }
+        return num==1;
+    }
+    bool isValid(string &s , int start , int cur){
+        return notleadZero(s , start, cur)&&power5(s, start, cur);
+    }
+    int solve(int idx , string&s ){
+        int n = s.size();
+        if(idx>=n){
+            return 0;
+        }
+        int ans = INT_MAX;
+        for(int i = idx ;i<n;i++){
+            if(isValid(s,idx, i )){
+                int temp = solve(i + 1, s);
+                if (temp != INT_MAX) { // Only consider valid partitions
+                    ans = min(ans, 1 + temp);
+                }
+            }
+           
+        }
+        return ans;
+    }
+public:
+    int minimumBeautifulSubstrings(string s) {
+       int ans = solve(0,s);
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
+    }
+};
