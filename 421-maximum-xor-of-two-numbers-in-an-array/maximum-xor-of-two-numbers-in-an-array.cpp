de229a34183f28088c7572294cb54f85d9c2e24e
ukmaurya <ukmaurya30@gmail.com>
2024-09-03 18:42:52 +0530

    Time: 653 ms (25.81%) | Memory: 244.4 MB (22.29%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,81 +1,72 @@
 struct Node{
-    Node *links[2];
-    
-    bool containsKey(int bit){
-          return links[bit]!=NULL;
+    Node* links[2];
+    bool flag = false;
+    bool containsKey(int key){
+        if(links[key]){
+            return true;
+        }
+        return false;
     }
-    void put(int bit , Node* node){
-        links[bit]= node;
-    }
-    Node* get(int bit){
+    Node* getKey(int bit){
         return links[bit];
     }
-
+    void put(int key , Node* node){
+         links[key] = node;
+    } 
+      
 };
 class Trie{
-    Node *root;
-    public :
-    Trie(){
-        root = new Node();
-    }
+    Node* root;
+    public:
+      Trie(){
+          root = new Node();
+      }
     void insert(int num){
-
-        Node* node = root;
-        for(int i=31 ;i>=0;i--){
-           // int bit = num&(1<<i); // why it is not working ? 
-            /*
-            If num = 29 (binary 11101) and i = 3:
-1 << 3 is 00001000 (binary).
-num & 00001000 is 00001000 (binary), which is 8 in decimal, indicating the 3rd bit is 1.
-*/
-           
-            
-            int bit = num&(1<<i);
-            if(bit !=0) // apply this if u want above logic
-                bit=1;
-            else bit = 0;
-            // int bit = (num>>i)&1;
-            if(!node->containsKey(bit)){
-                 node->put(bit, new Node());
-            }
-            node = node->get(bit);
-        }
-    }
-    int getMax(int num){
+       
         Node *node = root;
-        int maxi=0;
-
         for(int i=31;i>=0;i--){
-            //int bit = num&(1<<i);
-            int bit = (num>>i)&1;
-            int req = 1-bit;
-            if(node->containsKey(req)){
-                
-                maxi = maxi|(1<<i);
-                node = node->get(req);
-              }
-            else{
-                 node = node->get(bit);
-            }
+            int bit = ( num>>i )&1;
+            if(!node->containsKey(bit)){
+                node->put(bit , new Node());
+             }
+            node = node->getKey(bit);
+            
         }
-        return maxi;
     }
+    int getMaxXor(int num){
+         Node *node = root;
+        int mask = 0;
+        for(int t=31;t>=0;t--){
+            int bit = (num>>t)&1;
+            int req = 1-bit;
+            if(node->containsKey(req) ){
+                mask = mask|(1<<t);
+                node = node->getKey(req);
+             }
+            else{
+                node = node->getKey(bit);
+            }
+           
+        }
+        return mask;
+    }
+    
 };
-
-
-
 class Solution {
 public:
-    int findMaximumXOR(vector<int>& nums) {
-        int n = nums.size();
-    Trie trie;
-    for(int i=0;i<n;i++){
-        trie.insert(nums[i]);
-    } 
-    int maxi = 0;
-    for(auto it : nums){
-        maxi = max(maxi , trie.getMax(it));
-    }
-    return maxi;
+    int findMaximumXOR(vector<int>& nums){
+       int n = nums.size();
+       Trie trie;
+       for(int i=0;i<n;i++){
+           // we need to insert the binary bits 
+           trie.insert(nums[i]);
+       }  
+       int maxi = INT_MIN;   
+        for(int i=0;i<n;i++){
+             int temp = trie.getMaxXor(nums[i]); // class elements are accessed by . but structure elements are accessed by ->
+             maxi = max(maxi , temp);
+        }
+        
+    return maxi ;    
     }
 };
