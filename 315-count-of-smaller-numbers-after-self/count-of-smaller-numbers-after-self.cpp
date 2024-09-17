ec02b9b027ffef08803999c33645dc2a7f3aafad
ukmaurya <ukmaurya30@gmail.com>
2024-09-17 21:47:46 +0530

    Time: 1012 ms (18.60%) | Memory: 509.4 MB (5.45%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,7 +1,8 @@
 class Solution {
     
     private:
-    void merge(int start, int mid , int end , vector<pair<int,int>>&arr, vector<int>&ans){
+  /*  void merge(int start, int mid , int end , vector<pair<int,int>>&arr , vector<int>&ans, {
+  vector<int>&ans){
         vector<pair<int,int>> temp;
         int i = start;
         int j= mid+1;
@@ -29,7 +30,46 @@ class Solution {
             arr[i+start].second = temp[i].second;
             
         }
+    } */
+    
+    // earlier function was correct but it was doing by sorting in decreasing order , now we will do by increasing order
+    void merge(int start , int mid , int end , vector<pair<int,int>>&arr , vector<int>&ans ){
+        
+        vector<pair<int,int>> temp;
+        int i = start;
+        int j = mid+1;
+        int numElemsRightArrayLessThanLeftArray = 0;
+        while(i<=mid && j<=end){
+            if(arr[i].first <= arr[j].first){
+                ans[arr[i].second] += numElemsRightArrayLessThanLeftArray;
+                temp.push_back({arr[i].first , arr[i].second});
+                i++;
+            }
+            else{
+                temp.push_back({arr[j].first , arr[j].second});
+                numElemsRightArrayLessThanLeftArray++;
+                j++;
+            }
+           
+        }
+        while(i<=mid){
+              ans[arr[i].second] += numElemsRightArrayLessThanLeftArray;
+              temp.push_back({arr[i].first , arr[i].second});
+              i++;
+        }
+        while(j<=end){
+             temp.push_back({arr[j].first , arr[j].second});
+                j++;
+        }
+        
+        for(int i=0;i<temp.size();i++){
+            arr[i+start].first = temp[i].first;
+            arr[i+start].second = temp[i].second;
+            
+        }
+        
     }
+        
     void mergeSort(int i, int j , vector<pair<int,int>> &arr , vector<int>&ans){
         if(i>=j)
             return;
