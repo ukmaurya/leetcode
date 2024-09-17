43e3a8a9481043281599319d9741c87d3f84b888
ukmaurya <ukmaurya30@gmail.com>
2024-09-17 18:50:03 +0530

    Time: 6 ms (41.06%) | Memory: 16.7 MB (45.73%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,10 +3,17 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         // approach 1 : '0' act as divider so we will use prefix and suffix logic
+        // take some cases like below 
+        /*
+           1)  -ve at begining 2) -ve at end
+           3) odd number of -ves 4) even number of -ves
+           4)  case 
+           
+        */
         double  pre = 1;
         double suff =1;
         double  n = nums.size();
-         double  ans = INT_MIN;
+        double  ans = INT_MIN;
         for(int i=0;i<n;i++){
             if(pre==0){ //
                 pre=1;
