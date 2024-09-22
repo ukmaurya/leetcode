0913402f15e05d6067dac7465111f74660011846
ukmaurya <ukmaurya30@gmail.com>
2024-09-22 13:05:33 +0530

    Time: 4 ms (76.05%) | Memory: 9.5 MB (62.83%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,62 +1,49 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-  
-        //  since we have to preserve the order also , so need of sorting 
-        // see "bbbaaaba" "aaabbbba" ans should be false if we do sorting 
-     /*    int n1 = s.size();
-         int n2 = t.size();
-         if(n1 != n2){
-             return false;
-         }
-         vector<int> freq1(256 ,0);
-         vector<int> freq2(256 , 0);
-         for(int i=0;i<n1;i++){
-             freq1[s[i]]++;
-         }
-         for(int j=0;j<n2;j++){
-             freq2[t[j]]++;
-         }
-        for(int i=0;i<n1;i++){
-            if(freq1[s[i]]!=freq2[i]){
+        
+        int n = s.size();
+        int m = t.size();
+        if(n!=m){
+            return false;
+        }
+     /*   unordered_map<char , int> mps;
+        unordered_map<char , int> mpt;
+        for(int i=0;i<n;i++){
+            mps[s[i]]++;
+            mpt[t[i]]++;
+        }
+        for(int i=0;i<n;i++){ // considering the order among the characters 
+            if(mps[s[i]] != mpt[t[i]]){
                 return false;
             }
         }
-        return true;  */
-        // giving wrong ans on "egg" and "add"
         
-        // since we have to preserve order and mapping 
-         int n1 = s.size();
-         int n2 = t.size();
-         if(n1 != n2){
-             return false;
-         }
-        unordered_map<char , char> mp1;
-        unordered_map<char , char > mp2;
-        for(int i =0;i<n1;i++){
-            if(mp1.find(s[i])!=mp1.end()){
-                if(t[i] != mp1[s[i]] ){ // mapped to differnet char 
-                    return false;
-                }
+        return true; */
+        
+        //above logic is failing on example s = "bbbaaaba"  t = "aaabbbba"
+        
+        // we will store the mapping amping the characters
+        unordered_map<char , char> mps;
+        unordered_map<char , char> mpt;
+        for(int i=0;i<n;i++){
+           if(mps.find(s[i])!=mps.end()){ // mapping of s characters to t characters
+               if(mps[s[i]] != t[i]){
+                   return false;
+               }
+           }
+           else{
+                mps[s[i]]=t[i];
+            }
+            if(mpt.find(t[i])!=mpt.end()){ // mapping of t characters to s characters 
+                 if(mpt[t[i]] != s[i]){
+                   return false;
+               }
             }
             else{
-                mp1[s[i]] = t[i];
+                mpt[t[i]]=s[i];
             }
         }
-        
-        for(int i =0;i<n2;i++){
-            if(mp2.find(t[i])!=mp2.end()){
-                if(s[i] != mp2[t[i]] ){ // mapped to differnet char 
-                    return false;
-                }
-            }
-            else{
-                mp2[t[i]] = s[i];
-            }
-        }
-        
         return true;
-        
-        
     }
 };
