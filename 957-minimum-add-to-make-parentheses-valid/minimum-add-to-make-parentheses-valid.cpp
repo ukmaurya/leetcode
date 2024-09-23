4ba95050230257f9aac85813fbb90293c802321f
ukmaurya <ukmaurya30@gmail.com>
2024-09-23 12:12:03 +0530

    Time: 3 ms (42.37%) | Memory: 7.8 MB (57.78%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -3,7 +3,7 @@ public:
     int minAddToMakeValid(string s) {
         
         int n = s.size();
-        stack<char> st;
+      /*  stack<char> st;
         for(int i=0;i<n;i++){
             if(s[i]=='('){
                 st.push('(');
@@ -18,7 +18,27 @@ public:
                 
             }
         }
-        return st.size();
+        return st.size(); */
         
+        // using constant space 
+        int open = 0;
+        int close = 0;
+       // int ans =0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                open++;
+            }
+            else{
+                if(s[i]==')'){
+                    if(open>0){
+                        open--;
+                    }
+                    else{
+                        close++;
+                    }
+                }
+            }
+        }
+        return open+close;
     }
 };
