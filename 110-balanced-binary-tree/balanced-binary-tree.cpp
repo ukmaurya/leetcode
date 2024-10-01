48987c5da0bdca24f9c45368a0065b2c4437a941
ukmaurya <ukmaurya30@gmail.com>
2024-10-01 16:58:21 +0530

    Time: 17 ms (12.78%) | Memory: 25.3 MB (7.90%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,56 +10,68 @@
  * };
  */
 class Solution {
-   /* int flag = true;
-    int solve(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-        int left=0;
-        int right= 0;
-        if(root->left){
-            left = solve(root->left);
-        }
-        if(root->right){
-            right = solve(root->right);
-        }
-        
-        if(abs(right-left) >=2 ){
-            flag = false;
-        }
-            
-        return max(left,right)+1;    
-        
-    } */
-   int solve(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-        
-        int left = solve(root->left);
-        int right = solve(root->right);
-        if(left==-1 || right==-1){ // subtree is not balanced
-           return -1;
-        }
-        if(abs(left-right)>1){ // current node is not balanced
-            return -1; 
-        }
-        return max(left,right)+1;//if balanced then return height
-     
-   }
-public:
-    bool isBalanced(TreeNode* root) {
-        // int ans =  solve(root);
-        // if(flag==true)
-        //     return true;
-        // return false;
-        
-        // without flag and pruning the recursive calls
-        // we need a bool return type function but for height we need a int so , we conseder a integer value as false and true;
-        int ans =  solve(root);
-        if(ans==-1){
+    private:
+    bool isleaves(TreeNode *node){
+        if(node->left || node->right){
             return false;
         }
+        return true;
+    }
+    unordered_map<TreeNode * , int > height;
+    void calHeight(TreeNode* root){
+          if(root==NULL)
+              return ;
+          stack<pair<TreeNode* , int >> st; // node , state
+          st.push({root , 1});
+          while(!st.empty()){
+              auto it = st.top();
+              st.pop();
+              if(it.second==1){
+                  if(isleaves(it.first)){
+                      height[it.first]=1;
+                  }
+                   st.push({it.first , 2});
+                  if(it.first->right)
+                   st.push({it.first->right,1});
+                  if(it.first->left)
+                    st.push({it.first->left , 1});
+                 
+              }
+              else if(it.second==2){
+                  height[it.first] = max(height[it.first->left] , height[it.first->right])+1;
+
+              }
+          }
+          
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        // doing iterative 
+        // steps 
+        //  calculating height of each node iteratively , in postorder faision becoz lrroot
+        //  check weather it is balanced or not 
+         if(root==NULL)
+            return true;
+        calHeight(root);
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+             auto it = q.front();
+             q.pop();
+            int leftH = 0;
+            int rightH = 0;
+             if(it->left){
+                q.push(it->left);
+                leftH = height[it->left];
+              }
+              if(it->right){
+                 q.push(it->right);
+                 rightH = height[it->right];
+              }
+            if(abs(leftH-rightH) > 1)
+                return false;
+           }
         return true;
         
     }
