ec1157197e60e50d748c995e79f25d52549277a6
ukmaurya <ukmaurya30@gmail.com>
2024-10-10 16:56:25 +0530

    Time: 3 ms (34.49%) | Memory: 7.7 MB (70.30%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,19 +15,37 @@ public:
     int uniquePaths(int m, int n) {
         //  return solve(n-1,m-1);
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-         dp[n-1][m-1]=1;
-         for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(i==n-1 &&j==m-1)
-                  continue;
-                int right = dp[i][j+1];
-                int left = dp[i+1][j];
-                dp[i][j] = left+right;
-            }
-         }
-        return dp[0][0];
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        //  dp[n-1][m-1]=1;
+        //  for(int i=n-1;i>=0;i--){
+        //     for(int j=m-1;j>=0;j--){
+        //         if(i==n-1 &&j==m-1)
+        //           continue;
+        //         int right = dp[i][j+1];
+        //         int left = dp[i+1][j];
+        //         dp[i][j] = left+right;
+        //     }
+        //  }
+        // return dp[0][0];
+  
+        // space optimisation 
 
+         vector<int> last(m+1 ,0);
+         last[m-1] =1;
+         for(int i=n-1;i>=0;i--){
+            vector<int> cur(m+1,0);
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    cur[j]=1;
+                    continue;
+                }
+                int right = cur[j+1];
+                int left = last[j];
+                cur[j] = left+right;
+            }
+             last = cur;
+         }           
+       return last[0];
 
     }
 };
