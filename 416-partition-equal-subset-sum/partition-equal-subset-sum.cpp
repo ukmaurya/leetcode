317d7d9001a505e824a2a904139d8ca3346fce34
ukmaurya <ukmaurya30@gmail.com>
2024-10-11 11:22:48 +0530

    Time: 284 ms (26.53%) | Memory: 16.3 MB (79.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -9,20 +9,38 @@ public:
         if(sum&1)
           return false;
         sum = sum/2;
-        vector<vector<bool>>dp(n+1, vector<bool>(sum+1,false));
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        for(int i=n-1;i>=0;i--){
+    //     vector<vector<bool>>dp(n+1, vector<bool>(sum+1,false));
+    //     for(int i=0;i<n;i++){
+    //         dp[i][0]=true;
+    //     }
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=1;j<=sum;j++){
+    //             bool take = false;
+    //             if(j-nums[i]>=0)
+    //                take = dp[i+1][j-nums[i]];
+    //             bool nottake = dp[i+1][j];
+    //             dp[i][j] = take  || nottake;
+    //         }
+    //     }
+
+    //    return dp[0][sum];
+
+        
+        //    space optimised 
+          
+           vector<bool>down(sum+1 ,false);
+           down[0] = true;
+            for(int i=n-1;i>=0;i--){
+                vector<bool> up(sum+1 , false);
             for(int j=1;j<=sum;j++){
                 bool take = false;
                 if(j-nums[i]>=0)
-                   take = dp[i+1][j-nums[i]];
-                bool nottake = dp[i+1][j];
-                dp[i][j] = take  || nottake;
-            }
-        }
-
-       return dp[0][sum];
+                   take = down[j-nums[i]];
+                bool nottake = down[j];
+                up[j] = take  || nottake;
+             }
+             down = up;
+           }
+          return down[sum];
     }
 };
