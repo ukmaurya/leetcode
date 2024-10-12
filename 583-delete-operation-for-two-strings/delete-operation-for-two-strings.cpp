7ca9b3fcc59acd22761b935ea81c70edd3efed9c
ukmaurya <ukmaurya30@gmail.com>
2024-10-12 17:43:48 +0530

    Time: 12 ms (62.79%) | Memory: 15.7 MB (78.44%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -26,31 +26,63 @@ public:
 
         // approach 2 
        
+       
         // return solve(0,0,word1 , word2);
-        int n = word1.size();
+        // int n = word1.size();
+        // int m = word2.size();
+        // vector<vector<int>>dp(n+1 , vector<int>(m+1 , 1e9));
+        // // inititalisaton
+        // dp[n][m]=0;
+        // for(int j=0;j<m;j++){
+        //    dp[n][j] = m-j;
+        // }
+        // for(int i=0;i<n;i++){
+        //    dp[i][m] = n-i;
+        // }
+
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=m-1;j>=0;j--){
+        //         if(word1[i]==word2[j]){
+        //             dp[i][j] = dp[i+1][j+1];
+        //         }
+        //         else{
+        //             dp[i][j] = 1+min(dp[i+1][j] , dp[i][j+1]);
+        //         }
+        //     }
+        // }
+
+        // return dp[0][0];
+
+        // space optimisation  care : here 2 side initialisation is there 
+         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n+1 , vector<int>(m+1 , 1e9));
-        // inititalisaton
-        dp[n][m]=0;
+        vector<int> last(m+1 , 0);
+        vector<int> right(n+1 , 0);
         for(int j=0;j<m;j++){
-           dp[n][j] = m-j;
+           last[j] = m-j;
         }
         for(int i=0;i<n;i++){
-           dp[i][m] = n-i;
+            right[i] = n-i;
         }
-
-        for(int i=n-1;i>=0;i--){
+         for(int i=n-1;i>=0;i--){
+            vector<int> cur(m+1 , 0);
             for(int j=m-1;j>=0;j--){
                 if(word1[i]==word2[j]){
-                    dp[i][j] = dp[i+1][j+1];
+                    cur[j] = last[j+1];
+                    if(j==m-1)
+                      cur[j] = right[i+1];
                 }
                 else{
-                    dp[i][j] = 1+min(dp[i+1][j] , dp[i][j+1]);
+                    int temp =  cur[j+1];
+                    if(j==m-1)
+                      temp = right[i];
+                    cur[j] = 1+min(last[j] , temp);
+                   
                 }
             }
+            last = cur;
         }
-
-        return dp[0][0];
+        return last[0];
 
     }
 };
