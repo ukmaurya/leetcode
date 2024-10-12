362ff42d80529edbdb9286ee7acfc67428f77afa
ukmaurya <ukmaurya30@gmail.com>
2024-10-12 19:36:14 +0530

    Time: 2658 ms (5.02%) | Memory: 431.7 MB (8.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -11,7 +11,8 @@ class Solution {
             return dp[i][j]=solve(i-1,j,s,t,dp)+solve(i-1,j-1,s,t,dp);
 
         }
-        return dp[i][j]=solve(i-1,j,s,t,dp);
+        else 
+          return dp[i][j]=solve(i-1,j,s,t,dp);
 
     }
 public:
@@ -19,23 +20,23 @@ public:
         int mod = 1e9+7;
         int n = s.size();
         int m = t.size();
-   /*     vector<vector<int>> dp(n+1, vector<int>(m+1, -1)); // DP table
-        return solve(n-1,m-1,s,t,dp); */
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1)); // DP table
+        return solve(n-1,m-1,s,t,dp); 
         
-        vector<vector<int>> dp(n+2, vector<int>(m+2, 0));
-        for(int i=0;i<=n;i++)
-            dp[i][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j] = (dp[i-1][j]+dp[i-1][j-1])%mod;
-                }
-                else{
-                     dp[i][j] = dp[i-1][j]%mod;
-                }
-            }
-        }
+        // vector<vector<int>> dp(n+2, vector<int>(m+2, 0));
+        // for(int i=0;i<=n;i++)
+        //     dp[i][0]=1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s[i-1]==t[j-1]){
+        //             dp[i][j] = (dp[i-1][j]+dp[i-1][j-1])%mod;
+        //         }
+        //         else{
+        //              dp[i][j] = dp[i-1][j]%mod;
+        //         }
+        //     }
+        // }
         
-        return dp[n][m];
+        // return dp[n][m];
     }
 };
