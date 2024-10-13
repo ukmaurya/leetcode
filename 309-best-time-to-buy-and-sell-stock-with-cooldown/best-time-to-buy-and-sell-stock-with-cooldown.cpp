7bda81df268462576c2ff43abc14801fcb7f462c
ukmaurya <ukmaurya30@gmail.com>
2024-10-13 11:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (74.17%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,22 +18,46 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
        // return solve(0,1,prices);
-        int n = prices.size();
-        vector<vector<int>>dp(n+2,vector<int>(3,0));
-        for(int idx=n-1;idx>=0;idx--){
+        // int n = prices.size();
+        // vector<vector<int>>dp(n+2,vector<int>(3,0));
+        // for(int idx=n-1;idx>=0;idx--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         int take = 0;
+        //         if(buy==1){
+        //             take = dp[idx+1][0]-prices[idx];
+        //         }
+        //         else{
+        //             take = dp[idx+2][1]+prices[idx];
+        //         }
+        //         int ntake = dp[idx+1][buy];
+        //         dp[idx][buy] = max(take , ntake);
+        //     }
+        // }
+        // return dp[0][1];
+
+
+         //space optimisation
+         int n = prices.size();
+         vector<int>down1(3,0);
+         vector<int>down2(3,0);
+         for(int idx=n-1;idx>=0;idx--){
+            vector<int>cur(3,0);
             for(int buy=0;buy<=1;buy++){
                 int take = 0;
                 if(buy==1){
-                    take = dp[idx+1][0]-prices[idx];
+                    take = down1[0]-prices[idx];
                 }
                 else{
-                    take = dp[idx+2][1]+prices[idx];
+                    take = down2[1]+prices[idx];
                 }
-                int ntake = dp[idx+1][buy];
-                dp[idx][buy] = max(take , ntake);
+                int ntake = down1[buy];
+                cur[buy] = max(take , ntake);
             }
+            down2 = down1;
+            down1 = cur;
+
         }
-        return dp[0][1];
-        
+
+        return down1[1];
     }
 };
