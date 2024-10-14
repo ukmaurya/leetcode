293a71281ec72721217cff03a52aee7f9c81ba43
ukmaurya <ukmaurya30@gmail.com>
2024-10-14 12:01:01 +0530

    Time: 7 ms (81.70%) | Memory: 11.1 MB (86.59%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
-    int solve(int idx , vector<int>&arr , int k , int maxi){
-        int n = arr.size();
-        if(idx>=n){
-            return 0;
-        }
-        int ans = 0;
-      
-        for(int i=idx;i<=min(idx+k-1,n-1);i++){
-               maxi = max(maxi , arr[i]);
-               int temp = maxi*(i-idx+1);
-               temp += solve(i+1 , arr , k , INT_MIN );
-               ans = max(ans , temp);
-        }
-
-        return ans;
-    }
+       int solve(int i, int j ,int k,  int maxi  , vector<int>&arr){
+        if(i>j){
+          return 0;
+       }
+       int ans = INT_MIN;
+       for(int l = i;l <= min(i+k-1,j);l++){
+           maxi = max(maxi,arr[l]);
+           int temp = solve(l+1,j,k,INT_MIN,arr)+(l-i+1)*maxi;
+           ans = max(ans , temp );  
+       }
+       return ans;
+}
+        
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
 
-       // return solve(0, arr, k , INT_MIN);
+      // return solve(0,arr.size()-1 ,k, INT_MIN,arr);
           int n = arr.size();
           vector<int> dp(n+1 , 0);
           for(int idx=n-1;idx>=0;idx--){
