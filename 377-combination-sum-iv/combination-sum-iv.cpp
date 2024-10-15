d78c1eeae8cea6bd3dad7042c28075ff8aeaa71b
ukmaurya <ukmaurya30@gmail.com>
2024-10-15 10:44:54 +0530

    Time: 4 ms (22.36%) | Memory: 8.2 MB (56.76%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   
+    int solve(vector<int>& nums,int target,vector<int>&dp){
+        if(target==0){
+            return 1;
+        }
+        if(target<0){
+            return 0;
+        }
+        if(dp[target]!=-1){
+            return dp[target];
+        }
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans+=solve(nums,target-nums[i],dp);
+        }
+        return dp[target]=ans;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        return solve(nums,target,dp);
+    }
+};
