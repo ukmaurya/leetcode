2c3787b9ab078f485c7752025d66a3241c3ddad5
ukmaurya <ukmaurya30@gmail.com>
2024-10-26 17:58:27 +0530

    Time: 13 ms (91.67%) | Memory: 22.8 MB (21.43%) - LeetSync

--- 2687-lexicographically-smallest-beautiful-string/lexicographically-smallest-beautiful-string.cpp
+++ 2687-lexicographically-smallest-beautiful-string/lexicographically-smallest-beautiful-string.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
     string smallestBeautifulString(string s, int k) {
-        int n = s.size();
-        string inp = s;
-        bool bad = true;
-        for(int i = n-1;i>=0;i--){
-            bool isPossible = true;
-            for(int j = i;j<n;j++){ /// will run 2-4 times at most
-                char atLeast = 'a';
-                if(j == i){
-                    atLeast = s[i]+1;
-                }
-                bool found = false;
-                for(int c = atLeast;c<=('a'+k-1);c++){ // will run 2-4 times at most
-                    if((j == 0 || s[j-1]!=c) && (j<=1 || s[j-2]!=c)){
-                        s[j] = c;
-                        found = true;
-                        break;
-                    }
-                }
-                if(!found) {isPossible = false; break;}
+      int n = s.size();
+    string original = s;
+    bool isImpossible = true;
+
+    // Start from the end of the string, moving backward
+    for (int i = n - 1; i >= 0; i--) {
+        bool canConstruct = true;
+        
+        // Attempt to build a valid beautiful string from position `i` onward
+        for (int j = i; j < n; j++) {
+            char minChar = 'a';
+            if (j == i) {
+                minChar = s[i] + 1; // Increment character for the current position
             }
-            if(isPossible) {bad = false; break;}
-            else{
-                for(int l = i;l<min(n,i+2);l++) s[i] = inp[i];
+            bool charFound = false;
+            
+            // Find the smallest character that avoids repetition conflicts
+            for (char c = minChar; c <= ('a' + k - 1); c++) {
+                if ((j == 0 || s[j - 1] != c) && (j <= 1 || s[j - 2] != c)) {
+                    s[j] = c;
+                    charFound = true;
+                    break;
+                }
+            }
+            
+            // If no suitable character was found, move to the next position
+            if (!charFound) {
+                canConstruct = false;
+                break;
             }
         }
-        if(bad) return "";
-        return s;
+
+        // If successful in constructing a beautiful string, break
+        if (canConstruct) {
+            isImpossible = false;
+            break;
+        } else {
+            // Restore original characters in the current segment if construction failed
+            for (int l = i; l < min(n, i + 2); l++) {
+                s[l] = original[l];
+            }
+        }
+    }
+
+    // Return empty string if no valid construction was found
+    if (isImpossible) return "";
+    return s;
     }
 };
