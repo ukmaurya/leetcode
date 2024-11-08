4fc0249576ea3f2a7cd9ea8bb7c9d7d0ce35dfd6
ukmaurya <ukmaurya30@gmail.com>
2024-11-08 10:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (13.09%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -7,37 +7,23 @@ public:
         int ans =-1;
         while(start <= end){
             int mid = start-(start-end)/2;
-            if((arr[mid]-(mid+1)) >= k){
-                ans = mid;
-                end = mid-1;
-            }
-            else{
+            if((arr[mid]-(mid+1)) < k){
+                ans = mid; // just greater or equal
                 start = mid+1;
             }
+            else{
+                end = mid-1;
+            }
+        }
+        if(ans==-1){
+            return k;
+        }
+        int rem = k-(arr[ans]-(ans+1));
+       
+        return arr[ans]+rem;
 
 
-        }
-        cout<<ans<<endl;
-        if(ans==-1){ // handling of edge cases 
-             if(start == 0){ // no missing element 
-                 return arr[n-1]+k;
-             }
-             else if(end==n-1){
-                int req = arr[n-1]-n;
-                return arr[n-1]+k-req;
-             }
-        }
-        if(ans == 0){
-             int miss = arr[0]-1;
-             if(miss==0){
-                return arr[n-1]+k;
-             }
-             else{
-                return k;
-             }
-        }
-        int prevMiss = arr[ans-1] - ans;
-        int req = k-prevMiss;
-        return arr[ans-1]+req;
+        
+
     }
 };
