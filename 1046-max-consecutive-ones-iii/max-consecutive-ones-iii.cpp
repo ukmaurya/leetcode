4f5b53369bdc83c090306949b2d8efa7742514a1
ukmaurya <ukmaurya30@gmail.com>
2024-11-17 15:34:49 +0530

    Time: 5 ms (11.12%) | Memory: 58.3 MB (18.37%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // consecutive keyword is giving hint that concept will of substring 
-         int n = nums.size();
-         int front = 0;
-         int end = 0;
-         int count = 0;
-         int maxi = 0;
-         while( end < n ){ // variable size sliding window 
-            if(nums[end]==0)
-              count++;
-
-            if(count<k){
-                maxi = max(maxi , end-front+1);
-                end++;
-            }
-            else if(count==k){
-                  maxi = max(maxi , end-front+1);
-                  end++;
-            }  
-            else if(count>k){
-                while(count > k){
-                    if(nums[front]==0)
-                      count--;
-                   
-                    front++;
-                    
+        int n = nums.size();
+        int i=0;
+        int j=0;
+        int ans = 0;
+        int count =0;
+        while(j<n){
+           if(nums[j]==0){
+             count++;
+           }
+          
+           if(count<=k){
+            ans = max(ans , j-i+1);
+            j++;
+           }
+           else{
+            while(count>k){
+                if(nums[i]==0){
+                    count--;
                 }
-                end++;
+                i++;
+             }
+             j++;
+           }
 
-            }
-             
-         }
-         return maxi ;
+        }
+        return ans;
     }
 };
