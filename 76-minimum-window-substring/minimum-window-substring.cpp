3a51c4a41cc72b23a8507a1e506daa7ee62569d2
ukmaurya <ukmaurya30@gmail.com>
2024-11-17 21:46:27 +0530

    Time: 15 ms (65.48%) | Memory: 10.8 MB (64.57%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,57 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.size() < t.size()) return "";
         int n = s.size();
         int m = t.size();
-        int index = -1;
-        int ans = INT_MAX;
-        unordered_map<char , int> mp;
-        
+        unordered_map<char , int > mp;
         for(int i=0;i<m;i++){
             mp[t[i]]++;
-           
         }
-        int distCount = mp.size();
+        int startIndex = -1;
+        int minLen = INT_MAX;
+        int matched = t.size(); // number of char to be matched 
         int i = 0;
         int j = 0;
         while(j<n){
-            if(mp.find(s[j]) != mp.end()){
-                  mp[s[j]]--;
-                  if(mp[s[j]]==0){
-                    distCount--;
-                  }
-               }
-           if(distCount==0){
-                 while(distCount==0){
-                     if( ans > j-i+1){
-                          index = i;
-                          ans = min(ans, j-i+1);
-                      }
-                    if(mp.find(s[i]) != mp.end()){
-                        mp[s[i]]++;
-                        if(mp[s[i]]==1)
-                            distCount++;
-                     }
-                   i++; 
-                 }
-               j++;
-              }
-            else if(distCount>0){
-                j++;
+            if(mp.find(s[j])!= mp.end()){ // char is present in string
+                mp[s[j]]--;
+                if(mp[s[j]]>=0)
+                   matched--;
+                
             }
+            if(matched>=1){ // more char to be matched 
+                   j++;
+            }
+            else{ // all character are matched 
+                  if(minLen > j-i+1){
+                         minLen = min(minLen , j-i+1);
+                         startIndex = i;
+                      }
+                  while(matched <= 0){
+                     if(mp.find(s[i]) != mp.end()){
+                       mp[s[i]]++;
+                      if(mp[s[i]]>0)
+                         matched++;
+                      if(minLen > j-i+1){
+                         minLen = min(minLen , j-i+1);
+                         startIndex = i;
+                      }
+                     }
+                     i++;
+                  }
+                  j++;
+            }
+
+
+        
+
         }
-        if(ans == INT_MAX){
+        if(startIndex==-1){
             return "";
         }
-        return s.substr(index , ans);
-    }
+        string ans = "";
+        ans = s.substr(startIndex , minLen);
+        return ans;
+
+  }
 };
