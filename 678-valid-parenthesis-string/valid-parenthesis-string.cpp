ddaaf3831ae67dba4dadda0fcc271a442ea52434
ukmaurya <ukmaurya30@gmail.com>
2024-11-19 13:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (27.03%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,44 +1,41 @@
 class Solution {
-
-    bool solve(int idx , string s , int openCount , int closeCount,vector<vector<vector<int>>>&dp){
-        int n = s.size();
-        if(idx == n){
-            if(openCount==closeCount){
-                return true;
-            }
-            return false;
-        }
-        if(openCount<closeCount){
-            return false;
-        }
-        if(dp[idx][openCount][closeCount]!=-1){
-            return dp[idx][openCount][closeCount];
-        }
-        if(s[idx]=='('){
-                return dp[idx][openCount][closeCount]=solve(idx+1 , s , openCount+1 , closeCount ,dp);
-            }
-            else if(s[idx]==')'){
-                return dp[idx][openCount][closeCount]=solve(idx+1 , s , openCount , closeCount+1,dp);
-            }
-            else{
-                bool temp1 = solve(idx+1 , s , openCount+1 , closeCount,dp);
-                bool temp2 = solve(idx+1 , s , openCount , closeCount+1,dp);
-                bool temp3 = solve(idx+1 , s , openCount , closeCount,dp);
-                if(temp1||temp2||temp3){
-                    return dp[idx][openCount][closeCount]=true;
-                }
-                return dp[idx][openCount][closeCount]=false;
-            }
-        
-           return dp[idx][openCount][closeCount]=true;
-    }
-    
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-         int openCount=0;
-          int closeCount=0;
-          vector<vector<vector<int>>>dp(n,vector<vector<int>>(n,vector<int>(n,-1)));
-         return solve(0,s ,openCount , closeCount,dp);
+          // we will maintain 2 stack , operand and * stack 
+          // and we will store the index instead of characters 
+          int n = s.size();
+          stack<int> st , star;
+          for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]=='*'){
+                star.push(i);
+            }
+            else{
+                if(st.empty() && star.empty()){
+                    return false;
+
+                }
+                else{
+                    if(!st.empty())
+                      st.pop();
+                    else{
+                        star.pop();
+                    }  
+                }
+            }
+          }
+          while(!st.empty()){
+              if(star.empty())
+                return false;
+              else if(st.top() > star.top()) // ( is present after start 
+                return false;  
+              else{
+                star.pop();
+                st.pop();
+              }  
+          }
+        return true;
     }
 };
