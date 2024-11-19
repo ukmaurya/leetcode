7236045b8e6e15ff57c5986523ad9b7ec37d96bc
ukmaurya <ukmaurya30@gmail.com>
2024-11-19 19:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (38.83%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,39 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        TreeNode *cur = root;
-        if(root==NULL)
-         return{};
-        while(cur){
-            TreeNode *temp = cur->left;
-            if(temp==NULL){
+        TreeNode* cur = root;
+        while (cur) {
+            TreeNode* temp = cur->left;
+            if (temp) {
+                while (temp->right != cur && temp->right) {
+                    temp = temp->right;
+                }
+                if(temp->right== cur){ // visiting second time 
+                    ans.push_back(cur->val);
+                    temp->right= NULL;
+                    cur = cur->right;
+                }
+                else{
+                    temp->right=cur;
+                    cur = cur->left;
+                }
+            } else {
                 ans.push_back(cur->val);
                 cur = cur->right;
-                
-            }
-            else{
-                while(temp->right!=NULL && temp->right != cur){
-                  temp = temp->right;
-                }
-              if(temp->right == NULL){
-                temp->right = cur;
-                cur = cur->left;
-               }
-             else{
-                temp->right=NULL;
-                   ans.push_back(cur->val);
-                cur = cur->right;
-              }
             }
         }
+
         return ans;
-        
     }
 };
