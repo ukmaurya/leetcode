31f3a8b8c25ccfb334e78ba7cf3c4ed706c0fc44
Umesh kumar <ukmaurya30@gmail.com>
2025-02-22 01:30:01 +0530

    Time: 8 ms (37.45%) | Memory: 113.8 MB (37.05%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
+        // extension of 3191 
+        // int n = nums.size();
+        // int flipCount = 0;
+        // int ans = 0;
+        // vector<bool> flip(n,false);
+        // for(int i=0;i<n;i++){
+        //      if(i>=k && flip[i-k]==true){
+        //         flipCount--;
+        //      }
+        //      if(flipCount%2 == nums[i]){ // to toogle bit
+        //           if(i+k>n){ // not possible 
+        //             return -1;
+        //           }
+        //           flipCount++;
+        //           ans++;
+        //           flip[i] = true;
+        //      }
+        // }
+        // return ans;
+
+
+
+        // using the deque => space o(k)
+        
         int n = nums.size();
         int flipCount = 0;
         int ans = 0;
-        vector<bool> flip(n,false);
+        deque<int> dq;
         for(int i=0;i<n;i++){
-             if(i>=k && flip[i-k]==true){
-                flipCount--;
+             if(i>=k){
+                 flipCount -= dq.front(); // 0 or 1 will be subtracted 
+                  dq.pop_front();
              }
              if(flipCount%2 == nums[i]){ // to toogle bit
                   if(i+k>n){ // not possible 
@@ -15,9 +40,15 @@ public:
                   }
                   flipCount++;
                   ans++;
-                  flip[i] = true;
+                  dq.push_back(1); // since we are flipping 
+                 
+             }
+             else{
+                 dq.push_back(0); // put 0 since we are not flipping 
              }
         }
         return ans;
+
+
     }
 };
